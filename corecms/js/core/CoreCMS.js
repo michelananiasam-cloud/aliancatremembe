cea642a4dffe34e8e19d375f51ad6814ec74be0f
michelananiasam-cloud <michel.ananias.am@gmail.com>
2026-07-11 15:12:20 -0300

    Add CoreCMS class with version and start method

--- corecms/js/core/CoreCMS.js
+++ corecms/js/core/CoreCMS.js
@@ -1,1 +1,11 @@
+// js/core/CoreCMS.js
 
+export default class CoreCMS {
+    constructor() {
+        this.version = "0.0.1";
+    }
+
+    async start() {
+        console.log(`🚀 CoreCMS v${this.version} iniciado.`);
+    }
+}
